26193f26c22776e1a548fedf08ab92fb569c5f60
Dubjay18 <jejeniyi7@gmail.com>
2023-11-28 21:59:09 +0100

    setter and getter

--- hello.java
+++ hello.java
@@ -2,7 +2,10 @@
 
 class Main {
     public static void main(String[] args) {
-     
-       System.out.println(args);
+     if(args.length < 1){
+        System.out.println("add your name");
+     }else{
+        System.out.println("Hello dear " + args[0]);
+     }
     }
 }
