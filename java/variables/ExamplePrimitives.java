1b4c17306c6cd68235d6662f6ea0f717fecac9fe
Dubjay18 <jejeniyi7@gmail.com>
2023-11-29 23:54:25 +0100

    primitives 1

---
+++ java/variables/ExamplePrimitives.java
@@ -0,0 +1,10 @@
+package java.variables;
+
+public class ExamplePrimitives{
+    public static void main(String[] args) {
+    byte b =  34;
+    short s =  23000;
+    int i =  45000;
+    long l =  1000000000000000000L;
+    }
+}
